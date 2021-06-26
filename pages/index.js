4d924fa1bdef2156a43905a82fc0233a80cf982a
Mithu12 <mosaddekalimithu123@gmail.com>
2021-06-26 22:23:18 +0600

    event list page, single event page updated

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
 import Layout from "@/components/Layout";
+import Link from 'next/link'
+import {API_URL} from "@/config/index";
+import EventItem from "@/components/EventItems";
 
-export default function Home() {
+export default function Home({events}) {
+    // console.log(events)
   return (
     <Layout>
-      <h1>Home page</h1>
+        <h1>Home</h1>
+        <h1>Upcoming Events</h1>
+        {events.length === 0 && <h3>No events to show</h3>}
+
+        {events.map((evt) => (
+            <EventItem key={evt.id} evt={evt} />
+        ))}
+
+        {events.length > 0 && (
+            <Link href='/events'>
+                <a className='btn-secondary'>View All Events</a>
+            </Link>
+        )}
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch(API_URL+'/api/events')
+    const events = await res.json()
+
+    return {
+        props:{events: events.slice(0, 3)},
+        // revalidate: 1,
+    }
 }
