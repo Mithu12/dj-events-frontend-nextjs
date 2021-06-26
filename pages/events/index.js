2a63d65f96e2a936c5da52f2f3507c54b9fe1934
Mithu12 <mosaddekalimithu123@gmail.com>
2021-06-26 18:56:46 +0600

    header and footer added

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,9 +2,9 @@ import Layout from "../../components/Layout";
 
 const Index = (props) => {
     return (
-        <div>
+        <Layout>
             events page
-        </div>
+        </Layout>
     )
 };
 
