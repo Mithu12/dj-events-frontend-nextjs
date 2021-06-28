e571953d85bce5536b5b72462746ba3382eb310f
Mithu12 <mosaddekalimithu123@gmail.com>
2021-06-28 23:59:16 +0600

    image upload

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,28 +1,31 @@
 import moment from 'moment'
-import {FaImage} from 'react-icons/fa'
-import {ToastContainer, toast} from 'react-toastify'
+import { FaImage } from 'react-icons/fa'
+import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import {useState} from 'react'
-import {useRouter} from 'next/router'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
-import {API_URL} from '@/config/index'
+import Modal from '@/components/Modal'
+import ImageUpload from '@/components/ImageUpload'
+import { API_URL } from '@/config/index'
 import styles from '@/styles/Form.module.css'
 
-export default function EditEventPage({evt}) {
-    const [values, setValues] = useState({
-        name: evt.name,
-        performers: evt.performers,
-        venue: evt.venue,
-        address: evt.address,
-        date: evt.date,
-        time: evt.time,
-        description: evt.description,
-    })
-    const [imagePreview, setImagePreview] = useState(
-        evt.image ? evt.image.formats.thumbnail.url : null
-    )
+export default function EditEventPage({ evt }) {
+  const [values, setValues] = useState({
+    name: evt.name,
+    performers: evt.performers,
+    venue: evt.venue,
+    address: evt.address,
+    date: evt.date,
+    time: evt.time,
+    description: evt.description,
+  })
+  const [imagePreview, setImagePreview] = useState(
+    evt.image ? evt.image.formats.thumbnail.url : null
+  )
+  const [showModal, setShowModal] = useState(false)
 
     const router = useRouter()
 
@@ -54,119 +57,133 @@ export default function EditEventPage({evt}) {
         }
     }
 
-    const handleInputChange = (e) => {
-        const {name, value} = e.target
-        setValues({...values, [name]: value})
-    }
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setValues({ ...values, [name]: value })
+  }
 
-    return (
-        <Layout title='Add New Event'>
-            <Link href='/events'>Go Back</Link>
-            <h1>Edit Event</h1>
-            <ToastContainer/>
-            <form onSubmit={handleSubmit} className={styles.form}>
-                <div className={styles.grid}>
-                    <div>
-                        <label htmlFor='name'>Event Name</label>
-                        <input
-                            type='text'
-                            id='name'
-                            name='name'
-                            value={values.name}
-                            onChange={handleInputChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='performers'>Performers</label>
-                        <input
-                            type='text'
-                            name='performers'
-                            id='performers'
-                            value={values.performers}
-                            onChange={handleInputChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='venue'>Venue</label>
-                        <input
-                            type='text'
-                            name='venue'
-                            id='venue'
-                            value={values.venue}
-                            onChange={handleInputChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='address'>Address</label>
-                        <input
-                            type='text'
-                            name='address'
-                            id='address'
-                            value={values.address}
-                            onChange={handleInputChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='date'>Date</label>
-                        <input
-                            type='date'
-                            name='date'
-                            id='date'
-                            value={moment(values.date).format('yyyy-MM-DD')}
-                            onChange={handleInputChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='time'>Time</label>
-                        <input
-                            type='text'
-                            name='time'
-                            id='time'
-                            value={values.time}
-                            onChange={handleInputChange}
-                        />
-                    </div>
-                </div>
+  const imageUploaded = async (e) => {
+    const res = await fetch(`${API_URL}/events/${evt.id}`)
+    const data = await res.json()
+    setImagePreview(data.image.formats.thumbnail.url)
+    setShowModal(false)
+  }
 
-                <div>
-                    <label htmlFor='description'>Event Description</label>
-                    <textarea
-                        type='text'
-                        name='description'
-                        id='description'
-                        value={values.description}
-                        onChange={handleInputChange}
-                    ></textarea>
-                </div>
+  return (
+    <Layout title='Add New Event'>
+      <Link href='/events'>Go Back</Link>
+      <h1>Edit Event</h1>
+      <ToastContainer />
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.grid}>
+          <div>
+            <label htmlFor='name'>Event Name</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='performers'>Performers</label>
+            <input
+              type='text'
+              name='performers'
+              id='performers'
+              value={values.performers}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='venue'>Venue</label>
+            <input
+              type='text'
+              name='venue'
+              id='venue'
+              value={values.venue}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='address'>Address</label>
+            <input
+              type='text'
+              name='address'
+              id='address'
+              value={values.address}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='date'>Date</label>
+            <input
+              type='date'
+              name='date'
+              id='date'
+              value={moment(values.date).format('yyyy-MM-DD')}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='time'>Time</label>
+            <input
+              type='text'
+              name='time'
+              id='time'
+              value={values.time}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
 
-                <input type='submit' value='Update Event' className='btn'/>
-            </form>
+        <div>
+          <label htmlFor='description'>Event Description</label>
+          <textarea
+            type='text'
+            name='description'
+            id='description'
+            value={values.description}
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
 
-            <h2>Event Image</h2>
-            {imagePreview ? (
-                <Image src={imagePreview} height={100} width={170}/>
-            ) : (
-                <div>
-                    <p>No image uploaded</p>
-                </div>
-            )}
+        <input type='submit' value='Update Event' className='btn' />
+      </form>
 
-            <div>
-                <button className='btn-secondary btn-icon'>
-                    <FaImage/> Set Image
-                </button>
-            </div>
-        </Layout>
-    )
+      <h2>Event Image</h2>
+      {imagePreview ? (
+        <Image src={imagePreview} height={100} width={170} />
+      ) : (
+        <div>
+          <p>No image uploaded</p>
+        </div>
+      )}
+
+      <div>
+        <button
+          onClick={() => setShowModal(true)}
+          className='btn-secondary btn-icon'
+        >
+          <FaImage /> Set Image
+        </button>
+      </div>
+
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+      </Modal>
+    </Layout>
+  )
 }
 
-export async function getServerSideProps({query: {id}}) {
-    const res = await fetch(`${API_URL}/events/${id}`)
-    const evt = await res.json()
+export async function getServerSideProps({ params: { id } }) {
+  const res = await fetch(`${API_URL}/events/${id}`)
+  const evt = await res.json()
 
-    return {
-        props: {
-            evt,
-        },
-    }
+  return {
+    props: {
+      evt,
+    },
+  }
 }
